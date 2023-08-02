0e3f71cc532e64e708b4beb1c69a4db2cedb9618
Mikhiel <mikhielbenji9@gmail.com>
2023-08-02 22:36:22 +0530

    Completed the task-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import DataStreamer, { ServerRespond } from './DataStreamer';
+import React, {Component} from 'react';
+import DataStreamer, {ServerRespond} from './DataStreamer';
 import Graph from './Graph';
-import './App.css';
+ import './App.css';
+import {setInterval} from "timers";
 
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
-  showGraph:boolean,
+    showGraph: boolean
 }
 
 /**
@@ -23,7 +24,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph:false,
+      showGraph: false
     };
   }
 
@@ -31,57 +32,58 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    if(this.state.showGraph){
-      return (<Graph data={this.state.data}/>)
-    }
+    if (this.state.showGraph) {
+            return (<Graph data={this.state.data}/>)
+        }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-  // Set the showGraph state to true before starting data streaming
-  let x=0;
-  const interval=setInterval(()=>{
-    DataStreamer.getData((serverResponds: ServerRespond[])=>{
-      this.setState({
-        data:serverResponds,
-        showGraph:true,
-      });
-    });
-    x++;
-    if(x>1000){
-      clearInterval(interval);
+        let x = 0;
+        const interval = setInterval(() => {
+            DataStreamer.getData((serverResponds: ServerRespond[]) => {
+                this.setState({
+                    data: serverResponds,
+                    showGraph: true
+                });
+            });
+            x++;
+            if (x > 1000) {
+                clearInterval(interval);
+            }
+        }, 100);
     }
-  },100);
-}
 
   /**
    * Render the App react component
    */
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Bank & Merge Co Task 2
-        </header>
-        <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
-            Start Streaming Data
-          </button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
-        </div>
-      </div>
-    )
-  }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Bank & Merge Co Task 2
+                </header>
+                <div className="App-content">
+                    <button className="btn btn-primary Stream-button"
+                        // when button is click, our react app tries to request
+                        // new data from the server.
+                        // As part of your task, update the getDataFromServer() function
+                        // to keep requesting the data every 100ms until the app is closed
+                        // or the server does not return anymore data.
+                            onClick={() => {
+                                this.getDataFromServer()
+                            }}>
+                        Start Streaming Data
+                    </button>
+                    <div className="Graph">
+                        {this.renderGraph()}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
-import { ServerRespond } from './DataStreamer';
+import React, {Component} from 'react';
+import {Table} from '@finos/perspective';
+import {ServerRespond} from './DataStreamer';
 import './Graph.css';
 
 /**
@@ -15,7 +15,7 @@ interface IProps {
  * This interface acts as a wrapper for Typescript compiler.
  */
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void,
+    load: (table: Table) => void,
 }
 
 /**
@@ -27,57 +27,54 @@ class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+       return React.createElement('perspective-viewer');
   }
 
 
   componentDidMount() {
-    // Get element to attach the table from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+        // Get element to attach the table from the DOM.
+        const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
-    };
+            stock: 'string',
+            top_ask_price: 'float',
+            top_bid_price: 'float',
+            timestamp: 'date',
+        };
 
     if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
-    }
-    if (this.table) {
+            this.table = window.perspective.worker().table(schema);
+        }
+        if (this.table) {
+            // Load the `table` in the `<perspective-viewer>` DOM reference.
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-      elem.setAttribute('view','y_line');
-      elem.setAttribute('column-pivots','["stock"]');
-      elem.setAttribute('row-pivots','["timestamp"]');
-      elem.setAttribute('column','["top_ask_price"]');
-      elem.setAttribute('aggregates',`{
-        "stock":"distinct count",
-        "top_ask_price":"avg",
-        "top_bid_price":"avg",
-        timestamp":"distinct count"
-      }`);
-      // Add more Perspective configurations here.
       elem.load(this.table);
+            elem.setAttribute("view", "y_line");
+            elem.setAttribute("column-pivots", '["stock"]');
+            elem.setAttribute("row_pivots", '["timestamp"]');
+            elem.setAttribute("columns", '["top_ask_price"]');
+            elem.setAttribute("aggregates",
+                '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+            );
     }
   }
 
   componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
-    if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
-        return {
-          stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
-          timestamp: el.timestamp,
-        };
-      }));
-    }
-  }
+        // Everytime the data props is updated, insert the data into Perspective table
+        if (this.table) {
+            // As part of the task, you need to fix the way we update the data props to
+            // avoid inserting duplicated entries into Perspective table again.
+            this.table.update(this.props.data.map((el: any) => {
+                // Format the data from ServerRespond to the schema
+                return {
+                    stock: el.stock,
+                    top_ask_price: el.top_ask && el.top_ask.price || 0,
+                    top_bid_price: el.top_bid && el.top_bid.price || 0,
+                    timestamp: el.timestamp,
+                };
+            }));
+        }
+     }
 }
 
 export default Graph;
