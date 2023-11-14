bc08a0c09b2aacf0aeea0abeefba4e76a2d9f8d4
Lucas Aramendy <yataylucas@gmail.com>
2023-11-14 13:00:04 -0300

    Implement act. in time real in Graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
@@ -14,7 +15,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -31,8 +32,18 @@ class Graph extends Component<IProps, {}> {
   }
 
   componentDidMount() {
+
+    //const elem = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
+    // ... otras configuraciones
+
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('column-pivots', '["stock"]');
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('columns', '["top_ask_price"]');
+    elem.setAttribute('aggregates', '{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
 
     const schema = {
       stock: 'string',
