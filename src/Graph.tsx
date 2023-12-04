9236d7e4b7c679e0d6cc1bd5397a39561defa5bc
Mohammed-Saleh-Ishaq <mohammedishaq0592@gmail.com>
2023-12-04 22:28:53 +0530

    Create Patch File

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -46,9 +46,14 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('colums','["top_ask_price"]');
+      elem.setAttribute('Aggregates', '{"stock": "distinct count","top_ask_price":"avg","top_bid_price": "avg", "timestamp": "distinct count"}');
+      
     }
   }
 
